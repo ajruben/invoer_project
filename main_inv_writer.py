0f82cda466391df646afdd612f7e2618ad1c7354
ajruben <aj.rubenswarts@gmail.com>
2024-03-05 10:36:13 +0100

    coordinaten voor wegvakken

--- main_inv_writer.py
+++ main_inv_writer.py
@@ -132,20 +132,26 @@ class GeopackageToINV(Geopackage):
             for columns in [[column for column in columns_with_filter if filter in column] for filter in period_filter]
         ]
         content = []
+        
         for column_order in columns_per_period:
             content.append(self.select_by_column_order('wegdeelGPP', order=column_order))
         subrecord_array = np.array([
             [[111111111] + [j + 1] + [content[j][i][k] for k in range(len(content[j][i]))] for j in range(3)]
             for i in range(len(Emmissienr))
         ])
+        coord_data = self._retrieve_coordinates_sql_line(layer_name='wegdeelGPP')
+
         hoofd_record_lines_3d = hoofd_record_lines[:, np.newaxis, :]
-        result_box = np.concatenate((hoofd_record_lines_3d, subrecord_array), axis=1)
+        #coord_data_3d = coord_data[:, np.newaxis, :]
+        result_box = np.concatenate((hoofd_record_lines_3d, subrecord_array, coord_data), axis=1)
+  
+
         result_array = np.reshape(result_box, (result_box.shape[0] * result_box.shape[1], result_box.shape[2]))
         modified_data = np.where(result_array == 314159265.0, "<0>",
                          np.where(result_array == 111111111.0, "<1>",
                                   np.where(result_array == 123456.0, '', result_array)))
-        
-        selected_indices = [i * 4 for i in range(int(len(modified_data)/4))] 
+      
+        selected_indices = [i * 6 for i in range(int(len(modified_data)/6))] 
                 
                 
         s2=time.time()
@@ -163,8 +169,11 @@ class GeopackageToINV(Geopackage):
                 # Round to two decimals for other numeric values
                 modified_data[idx][2:6] = [round(float(value), 2) if value else '' for value in modified_data[idx][2:6]]
                 modified_data[idx][6:10] = [int(round(float(value))) if value else '' for value in modified_data[idx][6:10]]
-                    
-                        #modified_data = [lst[:-2] if i in selected_indices else lst for i, lst in enumerate(modified_data)]
+            for k in range(4,6):
+                idx = i + k
+                modified_data[idx] = [round(float(value), 2) if value else '' for value in modified_data[idx]]
+
+                #modified_data = [lst[:-2] if i in selected_indices else lst for i, lst in enumerate(modified_data)]
         
 
         modified_data = modified_data.tolist()
@@ -188,7 +197,7 @@ class GeopackageToINV(Geopackage):
             np.savetxt(file, emissie_line, header="EMISSIE", fmt=['%d'] * 2 + ['%.2f'] * 8, delimiter=', ', comments='', newline='\n<0>, ')
         self._remove_trailing_0()
         self._remove_trailing_commas(temp_file)
-        self._retrieve_coordinates_sql_line(layer_name='wegdeelGPP')
+        
         
 
     #----------------------------------------------------------------------------------------------------
@@ -201,7 +210,6 @@ class GeopackageToINV(Geopackage):
             #-------reflecties
             contains_spectrum_column = [column_name for column_name in columns if spectrum_column in column_name]
             contains_spectrum_column
-            #print(columns, contains_spectrum_column)
         spectrum_content = self.select_by_column_order(layer_name, order=contains_spectrum_column)
  
         unique = list(enumerate(set(map(tuple, spectrum_content)))) # Outer things are to format data; set matters: gives back unique values
@@ -232,10 +240,20 @@ class GeopackageToINV(Geopackage):
         corners_needed = tuple(most_corners - corners for corners in line_corners)
     
         from itertools import chain #to unpack tuple
-        line_padded_tup = [(line_data[0],) + tuple(chain(*line_data[1])) + (31415, -31415, 31415, -31415) * pad_factor for pad_factor, line_data in zip(corners_needed, line_coordinates_tup)]
+        line_padded_tup = [tuple(chain(*line_data[1])) + (31415, -31415, 31415, -31415) * pad_factor for pad_factor, line_data in zip(corners_needed, line_coordinates_tup)]
         #Convert to arrays for vectorized operations:
         coord_data = np.array(np.array(line_padded_tup).tolist())
-        print(result_gen[-3:-1], "\n\n", results_gen[-3:-1],"\n\n", line_coordinates_gen[-3:-1], "\n\n",line_coordinates_tup[-3:-1], coord_data[-3:-1])
+        x_mask = [i for i in range(0, len(coord_data[0]), 4)]
+        y_mask = [i for i in range(1, len(coord_data[0]), 4)]
+        z_mask = [i for i in range(2, len(coord_data[0]), 4)]
+        m_mask = [i for i in range(3, len(coord_data[0]), 4)]
+        x_columns = coord_data[:,x_mask]
+        y_columns = coord_data[:,y_mask]
+        z_columns = coord_data[:,z_mask]
+        m_columns = coord_data[:,m_mask]
+        coord_data = np.dstack((x_columns, y_columns, z_columns) + 7*(m_columns,))
+        return coord_data
+    
 
     def _retrieve_coordinates_sql_poly(self, reflectie, layer_name): #TODO:
         """
@@ -289,6 +307,7 @@ class GeopackageToINV(Geopackage):
         prep_combine = np.column_stack((combined, zero_column))
         flag_column = np.repeat(9999993141592, coord_data.shape[0])
         headerline = np.column_stack((flag_column, prep_combine[:, [0, -2, -1]]))
+        
         #try create coords part:
         x_mask = [i for i in range(1, len(coord_data[0]), 4)]
         y_mask = [i for i in range(2, len(coord_data[0]), 4)]
@@ -332,7 +351,6 @@ class GeopackageToINV(Geopackage):
         headerline_3d = headerline[:, np.newaxis, :]
         result_array = np.concatenate((headerline_3d, coord_box), axis=1)
         flat_matrix = np.reshape(result_array, (result_array.shape[0] * result_array.shape[1], result_array.shape[2]))
-        # Reshape coord_box into a 2D matrix
         pattern = np.array([31415, -31415, 31415, -31415])
         matches = np.all(flat_matrix == pattern, axis=1)
         filtered_matrix = flat_matrix[~matches]
@@ -360,23 +378,20 @@ class GeopackageToINV(Geopackage):
             file.truncate() 
 
     def _remove_trailing_commas(self, temp_file):
-    
-
         with open(temp_file, 'r') as file:
             lines = file.readlines()
-    
-        selected_indices = [(i * 4) - 3 for i in range(int(len(lines)/4))] 
-    
-
+        selected_indices = [(i * 6) - (6 - 1) for i in range(int(len(lines)/6))] 
         for i in selected_indices:
-            values = lines[i].strip().split(',')
-            lines[i] = ','.join(values[:-2]) + '\n'
+            values1 = lines[i].strip().split(',')
+            values2 = lines[i+4].strip().split(',')
+            values3 = lines[i+5].strip().split(',')
+            lines[i] = ','.join(values1[:-2]) + '\n'
+            lines[i+4] = ','.join(values2[:5]) + '\n'
+            lines[i+5] =  ','.join(values3[:5]) + '\n'
         # Write the modified lines back to the file
         with open(self.inv_file_location, 'a') as file:
             file.writelines(lines)
         os.remove(temp_file)
-
-        
     geopackage = None
 
 
